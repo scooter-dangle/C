fa85aaa81f2591cd18b48be98723f19eeee5e3b6
scooter-dangle <scottlsteele@gmail.com>
2011-09-23 09:34:05 +0000

    minor

--- a10p.c
+++ a10p.c
@@ -17,6 +17,8 @@
 #define BUFFER_SIZE 1000
 
 // Prototypes
+char numToLetter(int);
+int letterToNum(char);
 int EncryptString(char*, int, int, int);
 int DecryptString(char*, int, int, int);
 int ModularInverse(int, int);
@@ -54,21 +56,75 @@ int main()
 }
 
 // Function implementations
+char numToLetter(int number) {
+	char letter = number + 97;
+	return letter;
+}
+
+int letterToNum(char letter) {
+	int number = letter - 97;
+	return number;
+}
+
 int EncryptString(char* encryptMe, int a, int b, int n) {
+	if (!CoprimeTest(a, b)) {
+		return FALSE;
+	}
+	int i;
+	for (i = 0; encryptMe[i] >= 97 && encryptMe[i] < 123; i++) {
+		int number = letterToNum(encryptMe[i]);
+		number = ((number*a) + b) % n;
+		while(number<0) number = (number + n) % n; // not really necessary
+												   // here, but makes function
+												   // easier to re-use
+		encryptMe[i] = numToLetter(number);
+	}
+	if (encryptMe[i]==0) { // Checking that next character is string
+						   // terminator
+		return TRUE;
+	}
+	else {
+		return FALSE;
+	}
 }
 
 
 int DecryptString(char* decryptMe, int a, int b, int n) {
+	if (!CoprimeTest(a, b)) {
+		return FALSE;
+	}
+	int i;
+	int a_inv = ModularInverse(a, n);
+	for (i = 0; decryptMe[i] >= 97 && decryptMe[i] < 123; i++) {
+		int number = letterToNum(decryptMe[i]);
+		number = (a_inv * (number-b)) % n;
+		while(number<0) number = (number + n) % n; // not really necessary
+												   // here, but makes function
+												   // easier to re-use
+		decryptMe[i] = numToLetter(number);
+	}
+	if (decryptMe[i]==0) { // Checking that next character is string
+						   // terminator
+		return TRUE;
+	}
+	else {
+		return FALSE;
+	}
 }
 
 
 int ModularInverse(int target, int mod) {
+	int remainder[3];		// Started based on wikipedia pseudocode
+	int auxiliary[3] = { 0, 1 };
+	return target;
 }
 
 
 int IsPrime(int amIPrime) {
+	return TRUE;
 }
 
 
 int CoprimeTest(int numOne, int numTwo) {
+	return TRUE;
 }
