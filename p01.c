41146758a92feb58de52f924cdb6b09a51631a4d
scooter-dangle <scottlsteele@gmail.com>
2011-09-24 20:06:37 -0400

    Completed non-extra-credit version of p01.c
    For extra credit, still need to
    * set up console text colors
    * validate user input
    * improve relevance of final output text / course score guidance

--- p01.c
+++ p01.c
@@ -9,6 +9,8 @@ int main(int argc, const char *argv[])
 {
 	// Variable declarations (damn you, MSVS! damn you and your non-c99-ness!)
 	// Getting all the inputs
+	printf( "CIS 111 Grade Calculator\n"                                      );
+	printf( "========================\n"                                      );
 	printf( "Instructions\nPlease enter your grade (in points) after each "   );
 	printf( "prompt and press the enter key.\n\n"                             );
 	int prog_assign_1;	// qjq
@@ -41,9 +43,47 @@ int main(int argc, const char *argv[])
 	// Calcurlashins
 	int sub_total;	// qjq
 	sub_total = prog_assign_1 + prog_assign_2 + prog_assign_3 + mid_term + attendance_and_participation;
+
+	int final_exam;	// qjq
+	final_exam = 150;
+
+	int aPts, bPts, cPts, dPts;		// qjq
+	float aPercent, bPercent, cPercent, dPercent;		// qjq
+
+	int min_a, min_b, min_c, min_d;		// qjq
+
+	min_a = 900;
+	min_b = 800;
+	min_c = 700;
+	min_d = 600;
+
+	aPts = min_a - sub_total;
+	bPts = min_b - sub_total;
+	cPts = min_c - sub_total;
+	dPts = min_d - sub_total;
+
+	aPercent = 100 * aPts / (float) final_exam;
+	bPercent = 100 * bPts / (float) final_exam;
+	cPercent = 100 * cPts / (float) final_exam;
+	dPercent = 100 * dPts / (float) final_exam;
+
 	// Output
 	printf( "                             Total Points So Far: %i", sub_total );
-	printf( "\n" );
+	printf( "\n\n\n" );
+
+	printf("Minimum final exam scores needed to earn each course grade\n"     );
+	printf("----------------------------------------------------------\n"     );
+	printf(" Course Grade   :   Points Needed   :   Percentage Needed\n"      );
+	printf("            A   :   %5i           :   %7.2f %%\n", aPts, aPercent );
+	printf("            B   :   %5i           :   %7.2f %%\n", bPts, bPercent );
+	printf("            C   :   %5i           :   %7.2f %%\n", cPts, cPercent );
+	printf("            D   :   %5i           :   %7.2f %%\n", dPts, dPercent );
+	printf( "\n\n" );
+
+	printf("Any score below %i points (%.2f%%) on the final",   dPts, dPercent);
+	printf(" exam will result in a\nfailing grade for the course.\n\n"        );
+	printf("NOTE: The Final Exam is worth %i points.\n", final_exam           );
+
 	return 0;
 }
 
